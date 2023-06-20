64311d06d4443da1b136405bfa6f5deef44d30ad
blase147 <solarmails2@gmail.com>
2023-06-20 19:16:13 +0100

    add deploy parameters

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_02_183425) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "adminpack"
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
